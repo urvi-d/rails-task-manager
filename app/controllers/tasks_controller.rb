96fda994adb4e59f8cd7a8e63c8c9c9e3cda5c06
Urvi Doshi <u.n.doshi@gmail.com>
2018-05-11 14:09:11 +0800

    add

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,7 +9,23 @@ class TasksController < ApplicationController
     # runs set_task
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+private
+
   def set_task
     @task = Task.find(params[:id])
   end
+end
+
+def task_params
+  params.require(:task).permit(:title, :details)
 end

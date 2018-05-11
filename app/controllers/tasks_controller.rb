ad727cccb59f124046efaec7e64eb6d001bd4de6
Urvi Doshi <u.n.doshi@gmail.com>
2018-05-11 12:51:12 +0800

    show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,15 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
+
   def index
     @tasks = Task.all
+  end
+
+  def show
+    # runs set_task
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
